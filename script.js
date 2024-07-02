6558c0cec8796f7c63290d3943a5eff8472ff5b0
Abideen Olafimihan <olafimihana@gmail.com>
2024-07-02 18:02:42 +0100

    all desktop work done

--- script.js
+++ script.js
@@ -13,8 +13,45 @@ slackEmail.dataset.testid = "slackEmail";
 const slackPicture = document.getElementById("picture");
 slackPicture.dataset.testid = "slackProfilePicture";
 
-console.log(slackName);
-console.log(currentTime);
-console.log(currentDay);
-console.log(slackEmail);
-console.log(slackPicture);
+const currentDayOfWeek = document.getElementById("currentDay");
+const currentTimeOfDay = document.getElementById("currentTime");
+
+const DATE = new Date();
+
+const dayOfWeek = DATE.getDay();
+
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const dayName = daysOfWeek[dayOfWeek];
+
+currentDayOfWeek.innerText = dayName;
+
+const getCurrentUTCTime = () => {
+  const date = new Date();
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const seconds = date.getUTCSeconds();
+  currentTimeOfDay.innerText = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+};
+
+// console.log("Current time in UTC:", getCurrentUTCTime());
+
+window.addEventListener("load", () => {
+  const interval = setInterval(() => {
+    getCurrentUTCTime();
+  }, 1000);
+});
