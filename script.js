69db2b07f3b5d78e43c01033aac7385f07e4b3db
Abideen Olafimihan <olafimihana@gmail.com>
2024-07-02 17:09:43 +0100

    desktop done

---
+++ script.js
@@ -0,0 +1,20 @@
+const slackName = document.getElementById("slackName");
+slackName.dataset.testid = "slackDisplayName";
+
+const currentTime = document.getElementById("time");
+currentTime.dataset.testid = "currentTimeUTC";
+
+const currentDay = document.getElementById("day");
+currentDay.dataset.testid = "currentDay";
+
+const slackEmail = document.getElementById("email");
+slackEmail.dataset.testid = "slackEmail";
+
+const slackPicture = document.getElementById("picture");
+slackPicture.dataset.testid = "slackProfilePicture";
+
+console.log(slackName);
+console.log(currentTime);
+console.log(currentDay);
+console.log(slackEmail);
+console.log(slackPicture);
